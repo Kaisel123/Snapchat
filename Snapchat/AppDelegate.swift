04a84662e12fcf42b7e8c13be6ce355ef24429be
Bruno David Alejo Apaza <bruno.alejo@tecsup.edu.pe>
2021-05-31 14:27:56 -0700

    Desarrollo de la tarea - Login con Google

--- Snapchat/AppDelegate.swift
+++ Snapchat/AppDelegate.swift
@@ -9,16 +9,22 @@
 import UIKit
 import CoreData
 import Firebase
+import GoogleSignIn
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        
+        // GOOGLE ACCOUNT
+        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
         return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance().handle(url) ?? false
     }
 
     // MARK: UISceneSession Lifecycle
--- Snapchat/iniciarSesionViewController.swift
+++ Snapchat/iniciarSesionViewController.swift
@@ -1,8 +1,12 @@
 import UIKit
 import Firebase
 import FirebaseAuth
+import GoogleSignIn
 
-class iniciarSesionViewController: UIViewController {
+class iniciarSesionViewController: UIViewController, GIDSignInDelegate {
+    
+    
+    
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
@@ -18,9 +22,26 @@ class iniciarSesionViewController: UIViewController {
         }
     }
     
+    @IBAction func loginGoogle(_ sender: Any) {
+        GIDSignIn.sharedInstance()?.signOut()
+        GIDSignIn.sharedInstance()?.signIn()
+    }
+    
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if error == nil && user.authentication != nil {
+            let credential = GoogleAuthProvider.credential(withIDToken: user.authentication.idToken,
+                                                           accessToken: user.authentication.accessToken)
+            Auth.auth().signIn(with: credential) { (result, error) in print("Intentando iniciar Sesion")
+                print("Logueado exitosamente con su cuenta de google - Correo: \(user.profile.email ?? "error al iniciar sesion, intente de nuevo")")
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    	
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+        GIDSignIn.sharedInstance()?.delegate = self
     }
 }
 
